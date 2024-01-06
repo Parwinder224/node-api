1abe10bda37f9ec2aa162fc28bfaa0339b0ba5a7
Parwinder <73394414+Parwinder224@users.noreply.github.com>
2024-01-06 11:48:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ jsonwebtoken = require("jsonwebtoken");
 // const PORT =process.env.PORT || 3000;
 var corsOptions = {
     origin: 'http://localhost:4200',
+    credentials:true,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
