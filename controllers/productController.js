015654cfffde872824d30111eecd9218fc82dc06
Parwinder <73394414+Parwinder224@users.noreply.github.com>
2024-01-06 11:45:14 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -16,7 +16,8 @@ const getProductByID = asyncHandler(async (req, res) => {
     try {
         const { id } = req.params;
         const product = await Product.findById(id)
-        res.status(200).json(product)
+        res.status(200).json(product);
+        res.set('Access-Control-Allow-Origin', 'http://localhost:4200');
     } catch (error) {
         // res.status(500).json({ message: error.message })
         res.status(500);
@@ -30,8 +31,8 @@ const addNewProduct = async (req, res) => {
     // res.send(req.body)
     try {
         const products = await Product.create(req.body);
-        res.status(200).json(products)
-
+        res.status(200).json(products);
+        res.set('Access-Control-Allow-Origin', 'http://localhost:4200');
     } catch (error) {
         console.log(error.message);
         res.status(500).json({ message: error.message })
@@ -47,7 +48,8 @@ const updateProduct = async (req, res) => {
             res.status(404).json({ message: `cannot find product with id ${id}` })
         }
         const updateProduct = await Product.findById(id);
-        res.status(200).json(updateProduct)
+        res.status(200).json(updateProduct);
+        res.set('Access-Control-Allow-Origin', 'http://localhost:4200');
     } catch (error) {
         res.status(500).json({ message: error.message })
 
@@ -63,7 +65,8 @@ const deleteProduct = async (req, res) => {
             res.status(404).json({ message: `cannot find product with id ${id}` })
         }
         // const updateProduct= await Product.findById(id);
-        res.status(200).json(product)
+        res.status(200).json(product);
+        res.set('Access-Control-Allow-Origin', 'http://localhost:4200');
     } catch (error) {
         res.status(500).json({ message: error.message })
 
