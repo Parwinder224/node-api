7503ddad80d5b04c76ecd745d5ebcd796dea913f
Parwinder <73394414+Parwinder224@users.noreply.github.com>
2024-01-06 19:14:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,15 @@ app.use(function(req, res, next) {
       next();
     }
   });
-app.use('/user',userRoute)
+app.use('/user',userRoute);
+app.use(function (req, res, next) {
+
+  res.header('Access-Control-Allow-Origin', "http://localhost:4200");
+  res.header('Access-Control-Allow-Headers', true);
+  res.header('Access-Control-Allow-Credentials', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
 
 
 
